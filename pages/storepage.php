f6c9afd920b3b18c73c7266f807ec2ef6c7c9ee9
Edoardo-Morosanu <edoardomorosanu05@gmail.com>
2024-10-09 19:56:27 +0200

    Added product list, closes #1

--- pages/storepage.php
+++ pages/storepage.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,50 +7,88 @@
     <link rel="stylesheet" href="../styles/normalize.css" type="text/css">
     <link rel="stylesheet" href="../styles/styles.css" type="text/css">
     <link rel="stylesheet" href="../styles/stylestorepage.css" type="text/css">
-    <link rel="icon" href="../assets/logos/png/sunny_logos_white.png" type="image/png">
 </head>
-
 <body>
     <div class="container">
-        <?php
-        $products = [
-            1 => [
-                "name" => "Classic Stripes",
-                "price" => "€6,75",
-                "original_price" => "€11,50",
-                "image" =>
-                "../assets/sunny_socks_photos/packaging/jpeg/catalogus_sokken_uni_green.jpg",
-            ],
-            2 => [
-                "name" => "Classic Stripes",
-                "price" => "€6,75",
-                "original_price" => "€11,50",
-                "image" =>
-                "../assets/sunny_socks_photos/packaging/jpeg/catalogus_sokken_uni_blue.jpg",
-            ],
-            3 => [
-                "name" => "Classic Stripes",
-                "price" => "€6,75",
-                "original_price" => "€11,50",
-                "image" =>
-                "../assets/sunny_socks_photos/packaging/jpeg/catalogus_sokken_uni_red.jpg",
-            ],
-        ];
-        foreach ($products as $product_id => $product) {
-            echo "
-                <div class='productbox'>
-                    <a href='productpage.php?product_id=$product_id'>
-                        <div class='image-container'><img src='{$product["image"]}' alt='{$product["name"]}' /></div>
-                        <h2>{$product["name"]}</h2>
-                        <p class='price'>
-                            <span class='discounted-price'>{$product["price"]}</span>
-                            <span class='original-price'>{$product["original_price"]}</span>
-                        </p>
-                    </a>
-                </div>";
-        }
-        ?>
+        <aside class="sidebar">
+            <div class="filters">
+            </div>
+        </aside>
+        <main class="main-content">
+            <h1>BRIGHT DEALS, BOLD SOCKS</h1>
+            <div class="product-grid">
+                <?php
+                $products = [
+                    [
+                        "name" => "Classic Stripes",
+                        "price" => "€6,75",
+                        "original_price" => "€11,50",
+                        "image" =>
+                            "../assets/sunny_socks_photos/packaging/jpeg/catalogus_sokken_uni_green.jpg",
+                    ],
+                    [
+                        "name" => "Classic Stripes",
+                        "price" => "€6,75",
+                        "original_price" => "€11,50",
+                        "image" =>
+                            "../assets/sunny_socks_photos/packaging/jpeg/catalogus_sokken_uni_blue.jpg",
+                    ],
+                    [
+                        "name" => "Classic Stripes",
+                        "price" => "€6,75",
+                        "original_price" => "€11,50",
+                        "image" =>
+                            "../assets/sunny_socks_photos/packaging/jpeg/catalogus_sokken_uni_pink.jpg",
+                    ],
+                    [
+                        "name" => "Classic Uni Color",
+                        "price" => "€6,75",
+                        "original_price" => "€11,50",
+                        "image" =>
+                            "../assets/sunny_socks_photos/packaging/jpeg/catalogus_sokken_uni_red.jpg",
+                    ],
+                    [
+                        "name" => "Classic Uni Color",
+                        "price" => "€6,75",
+                        "original_price" => "€11,50",
+                        "image" =>
+                            "../assets/sunny_socks_photos/packaging/jpeg/catalogus_sokken_uni_yellow.jpg",
+                    ],
+                    [
+                        "name" => "Classic Uni Color",
+                        "price" => "€6,75",
+                        "original_price" => "€11,50",
+                        "image" =>
+                            "../assets/sunny_socks_photos/packaging/jpeg/catalogus_sokken_uni_blue.jpg",
+                    ],
+                ];
+
+                foreach ($products as $product_id => $product): ?>
+                    <div class="product-item">
+                        <a href="productpage.php?product_id=<?= $product_id +
+                            1 ?>">
+                        <div class="image-container">
+                            <img src="<?= $product[
+                                "image"
+                            ] ?>" alt="<?= $product["name"] ?>" loading="lazy"/>
+                        </div>
+                        </a>
+                        <div class="product-details">
+                            <h2><?= $product["name"] ?></h2>
+                            <p class="price">
+                                <span class="discounted-price"><?= $product[
+                                    "price"
+                                ] ?></span>
+                                <span class="original-price"><?= $product[
+                                    "original_price"
+                                ] ?></span>
+                            </p>
+                        </div>
+                    </div>
+                <?php endforeach;
+                ?>
+            </div>
+        </main>
     </div>
 </body>
-
 </html>
