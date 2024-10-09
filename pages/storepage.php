f70a1dd7bc230c31a9a91cddec00c31fe9a97534
frederic-cvs-nhlstenden <frederic.cahn.von.seelen@student.nhlstenden.com>
2024-10-09 22:27:04 +0200

    Renamed global stylesheet, removed global grid and set min height+width

--- pages/storepage.php
+++ pages/storepage.php
@@ -1,13 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Store</title>
     <link rel="stylesheet" href="../styles/normalize.css" type="text/css">
-    <link rel="stylesheet" href="../styles/styles.css" type="text/css">
+    <link rel="stylesheet" href="../styles/global.css" type="text/css">
     <link rel="stylesheet" href="../styles/stylestorepage.css" type="text/css">
 </head>
+
 <body>
     <div class="container">
         <aside class="sidebar">
@@ -24,64 +26,58 @@
                         "price" => "€6,75",
                         "original_price" => "€11,50",
                         "image" =>
-                            "../assets/sunny_socks_photos/packaging/jpeg/catalogus_sokken_uni_green.jpg",
+                        "../assets/sunny_socks_photos/packaging/jpeg/catalogus_sokken_uni_green.jpg",
                     ],
                     [
                         "name" => "Classic Stripes",
                         "price" => "€6,75",
                         "original_price" => "€11,50",
                         "image" =>
-                            "../assets/sunny_socks_photos/packaging/jpeg/catalogus_sokken_uni_blue.jpg",
+                        "../assets/sunny_socks_photos/packaging/jpeg/catalogus_sokken_uni_blue.jpg",
                     ],
                     [
                         "name" => "Classic Stripes",
                         "price" => "€6,75",
                         "original_price" => "€11,50",
                         "image" =>
-                            "../assets/sunny_socks_photos/packaging/jpeg/catalogus_sokken_uni_pink.jpg",
+                        "../assets/sunny_socks_photos/packaging/jpeg/catalogus_sokken_uni_pink.jpg",
                     ],
                     [
                         "name" => "Classic Uni Color",
                         "price" => "€6,75",
                         "original_price" => "€11,50",
                         "image" =>
-                            "../assets/sunny_socks_photos/packaging/jpeg/catalogus_sokken_uni_red.jpg",
+                        "../assets/sunny_socks_photos/packaging/jpeg/catalogus_sokken_uni_red.jpg",
                     ],
                     [
                         "name" => "Classic Uni Color",
                         "price" => "€6,75",
                         "original_price" => "€11,50",
                         "image" =>
-                            "../assets/sunny_socks_photos/packaging/jpeg/catalogus_sokken_uni_yellow.jpg",
+                        "../assets/sunny_socks_photos/packaging/jpeg/catalogus_sokken_uni_yellow.jpg",
                     ],
                     [
                         "name" => "Classic Uni Color",
                         "price" => "€6,75",
                         "original_price" => "€11,50",
                         "image" =>
-                            "../assets/sunny_socks_photos/packaging/jpeg/catalogus_sokken_uni_blue.jpg",
+                        "../assets/sunny_socks_photos/packaging/jpeg/catalogus_sokken_uni_blue.jpg",
                     ],
                 ];
 
                 foreach ($products as $product_id => $product): ?>
                     <div class="product-item">
                         <a href="productpage.php?product_id=<?= $product_id +
-                            1 ?>">
-                        <div class="image-container">
-                            <img src="<?= $product[
-                                "image"
-                            ] ?>" alt="<?= $product["name"] ?>" loading="lazy"/>
-                        </div>
+                                                                1 ?>">
+                            <div class="image-container">
+                                <img src="<?= $product["image"] ?>" alt="<?= $product["name"] ?>" loading="lazy" />
+                            </div>
                         </a>
                         <div class="product-details">
                             <h2><?= $product["name"] ?></h2>
                             <p class="price">
-                                <span class="discounted-price"><?= $product[
-                                    "price"
-                                ] ?></span>
-                                <span class="original-price"><?= $product[
-                                    "original_price"
-                                ] ?></span>
+                                <span class="discounted-price"><?= $product["price"] ?></span>
+                                <span class="original-price"><?= $product["original_price"] ?></span>
                             </p>
                         </div>
                     </div>
@@ -91,4 +87,5 @@
         </main>
     </div>
 </body>
+
 </html>
